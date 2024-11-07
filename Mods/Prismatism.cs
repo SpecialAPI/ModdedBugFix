002b4a5c39ad0be80554d913987b9b372c295e55
SpecialAPI <pavlov.andrei.d@gmail.com>
2024-11-07 13:40:55 +0300

    Add Maiden Plating fix

--- Mods/Prismatism.cs
+++ Mods/Prismatism.cs
@@ -1,5 +1,6 @@
 ﻿using Dungeonator;
 using HarmonyLib;
+using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,10 @@ namespace ModdedBugFix.Mods
     {
         public static MethodInfo pff_cbu_p = AccessTools.Method(typeof(Prismatism), nameof(ParrotsFeatherFix_CanBeUsed_Prefix));
 
-        public static MethodInfo bf_ec_p = AccessTools.Method(typeof(Prismatism), nameof(Prefix));
+        public static MethodInfo bf_ec_p = AccessTools.Method(typeof(Prismatism), nameof(BraveryFix_EnemiesCheck_Prefix));
+
+        public static MethodInfo mpf_oph_t = AccessTools.Method(typeof(Prismatism), nameof(MaidenPlatingFix_OnPlayerHit_Transpiler));
+        public static MethodInfo mpf_oph_rp = AccessTools.Method(typeof(Prismatism), nameof(MaidenPlatingFix_OnPlayerHit_ReplaceProjectile));
 
         public static void Patch()
         {
@@ -35,6 +39,15 @@ namespace ModdedBugFix.Mods
                 if (enemiesCheck != null)
                     Plugin.HarmonyInstance.Patch(enemiesCheck, prefix: new(bf_ec_p));
             }
+
+            var maidenPlatingClass = AccessTools.TypeByName("katmod.MaidenPlating");
+            if(maidenPlatingClass != null)
+            {
+                var onPlayerHit = AccessTools.Method(maidenPlatingClass, "OnPlayerHit");
+
+                if (onPlayerHit != null)
+                    Plugin.HarmonyInstance.Patch(onPlayerHit, ilmanipulator: new(mpf_oph_t));
+            }
         }
 
         public static bool ParrotsFeatherFix_CanBeUsed_Prefix(ref bool __result, PlayerController user)
@@ -48,7 +61,7 @@ namespace ModdedBugFix.Mods
             return true;
         }
 
-        public static bool Prefix(PassiveItem __instance)
+        public static bool BraveryFix_EnemiesCheck_Prefix(PassiveItem __instance)
         {
             if (__instance == null || __instance.Owner == null || __instance.Owner.CurrentRoom == null)
                 return false;
@@ -57,6 +70,22 @@ namespace ModdedBugFix.Mods
                 return false;
 
             return true;
+        }
+
+        public static void MaidenPlatingFix_OnPlayerHit_Transpiler(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchCallOrCallvirt<BraveBehaviour>($"get_{nameof(BraveBehaviour.projectile)}")))
+                return;
+
+            crs.Emit(OpCodes.Ldloc_2);
+            crs.Emit(OpCodes.Call, mpf_oph_rp);
+        }
+
+        public static Projectile MaidenPlatingFix_OnPlayerHit_ReplaceProjectile(Projectile _, Projectile proj)
+        {
+            return proj;
         }
     }
 }
