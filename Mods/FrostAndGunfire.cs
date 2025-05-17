2e1aaf768d6154cc9e1755f235968a77916cf76c
SpecialAPI <pavlov.andrei.d@gmail.com>
2025-05-17 18:26:40 +0300

    Add a fix for Frost and Gunfire enemies having collision while spawning

--- Mods/FrostAndGunfire.cs
+++ Mods/FrostAndGunfire.cs
@@ -22,6 +22,28 @@ namespace ModdedBugFix.Mods
 
         public static MethodInfo bkf_cbu_p = AccessTools.Method(typeof(FrostAndGunfire), nameof(BloodiedKeyFix_CanBeUsed_Prefix));
 
+        public static MethodInfo egf_p = AccessTools.Method(typeof(FrostAndGunfire), nameof(EnemyGeneralFix_Postfix));
+
+        public static Dictionary<string, string> Enemies = new()
+        {
+            ["SuppressorShroom"]    = "kp:suppores",
+            ["Observer"]            = "kp:observer",
+            ["Gazer"]               = "kp:gazer",
+            ["Globbulon"]           = "kp:globbulon",
+            ["Bubbulon"]            = "kp:bubbulon",
+            ["Humphrey"]            = "kp:humphrey",
+            ["Ophaims"]             = "kp:ophaim",
+            ["Mushboom"]            = "kp:mushboom",
+            ["Firefly"]             = "kp:firefly",
+            ["Spitter"]             = "kp:gunzooka",
+            ["Spitfire"]            = "kp:spitfire",
+            ["Salamander"]          = "kp:salamander",
+            ["MiniMushboom"]        = "kp:mini_mushboom",
+            ["Shellet"]             = "kp:skell",
+            ["Silencer"]            = "kp:suppressor",
+            ["CannonKin"]           = "kp:cannon_kin",
+        };
+
         public const string Guid = "kp.etg.frostandgunfire";
         public static readonly Version SupportedVersion = new(1, 0, 0);
 
@@ -74,8 +96,84 @@ namespace ModdedBugFix.Mods
                 "MirrorGuon"
             };
 
+            foreach(var className in Enemies.Keys)
+            {
+                var enemyClass = AccessTools.TypeByName($"FrostAndGunfireItems.{className}");
+
+                if (enemyClass == null)
+                    continue;
+
+                var buildPrefab = AccessTools.Method(enemyClass, "BuildPrefab");
+
+                if (buildPrefab != null)
+                    Plugin.HarmonyInstance.Patch(buildPrefab, postfix: new(egf_p));
+
+                EnemyGeneralFix_Postfix(buildPrefab);
+            }
+
             foreach (var i in itemsWithBrokenOnDestroy)
                 OnDestroyGeneralFix.FixOnDestroy($"FrostAndGunfireItems.{i}");
+        }
+
+        public static void EnemyGeneralFix_Postfix(MethodBase __originalMethod)
+        {
+            if (__originalMethod?.DeclaringType?.Name is not string className || !Enemies.TryGetValue(className, out var enemyConsoleId))
+                return;
+
+            if (!Game.Enemies.ContainsID(enemyConsoleId))
+                return;
+
+            var enm = Game.Enemies[enemyConsoleId];
+
+            if (enm == null)
+                return;
+
+            if (enm.spriteAnimator is tk2dSpriteAnimator anim && anim != null && anim.Library != null && anim.GetClipByName("awaken") == null && anim.GetClipByName("spawn") == null)
+            {
+                var idles = new string[]
+                {
+                    "idle",
+                    "idle_left",
+                    "idle_right"
+                };
+                var sid = -1;
+                tk2dSpriteCollectionData coll = null;
+
+                foreach(var anmName in idles)
+                {
+                    var clippy = anim.GetClipByName(anmName);
+
+                    if(clippy == null || clippy.frames == null || clippy.frames.Length <= 0)
+                        continue;
+
+                    if (clippy.frames[0] is not tk2dSpriteAnimationFrame frame)
+                        continue;
+
+                    sid = frame.spriteId;
+                    coll = frame.spriteCollection;
+                    break;
+                }
+
+                if(sid != -1 && coll != null)
+                {
+                    anim.Library.clips = anim.Library.clips.AddToArray(new()
+                    {
+                        fps = 100,
+                        name = "awaken",
+                        wrapMode = tk2dSpriteAnimationClip.WrapMode.Once,
+                        frames =
+                        [
+                            new()
+                            {
+                                spriteCollection = coll,
+                                spriteId = sid,
+                            }
+                        ]
+                    });
+                }
+            }
+
+            enm.gameObject.GetOrAddComponent<ObjectVisibilityManager>();
         }
 
         public static bool BarterFix_CanBeUsed_Prefix(ref bool __result, PlayerController user)
